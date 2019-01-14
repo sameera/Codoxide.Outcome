ff9ecb6962ff34d82059407b1dd64bb18e6cccd2
Sameera <www.sameera@gmail.com>
2019-01-14 08:31:20 +0530

    Test cases for Continue handler

--- test/UnitTest.Codoxide.Outcome/When Outcomes fail/Given there is a continue handlers.cs
+++ test/UnitTest.Codoxide.Outcome/When Outcomes fail/Given there is a continue handlers.cs
@@ -8,32 +8,33 @@ namespace _.When_Outcomes_fail
     public class Given_there_is_a_catch_handlers
     {
         [Fact]
-        public void It_lets_the_catch_handler_to_return_a_differently_type_result()
+        public void It_lets_the_continue_handler_to_return_a_differently_typed_result()
         {
             this.GetFailingOutcome()
-                .Catch(failure => 100)
+                .Continue((result, failure) => 100)
                 .Then(value => {
                     value.Should().Be(100);
                 });
         }
 
         [Fact]
-        public async Task It_lets_the_asycn_catch_handler_to_return_a_differently_type_result()
+        public async Task It_lets_the_asycn_continue_handler_to_return_a_differently_typed_result()
         {
             await this.GetFailingOutcomeAsync()
-                .Catch(failure => 1000)
+                .Continue((result, failure) => 1000)
                 .Then(value => value.Should().Be(1000));
         }
 
         [Fact]
-        public async Task It_supports_async_operations_in_catch_handler()
+        public async Task It_supports_async_operations_in_continue_handler()
         {
             await this.GetFailingOutcomeAsync()
-                .Catch(failure => Task.FromResult(1000))
+                .Continue((result, failure) => Task.FromResult(1000))
                 .Then(value => value.Should().Be(1000));
         }
 
         public Outcome<string> GetFailingOutcome() => Outcome<string>.Reject("Failure!");
         public Task<Outcome<string>> GetFailingOutcomeAsync() => Task.FromResult(Outcome<string>.Reject("Failure!"));
+
     }
 }
